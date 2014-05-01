4299ae67c2af7f301c629dc5d0b3766af6943239
Javad Afshar <mjafshar@gmail.com>
2014-04-30 23:07:28 -0500

    initial commit.

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.GeLoCordovaPlugin/www/GeLoCordovaPlugin.js",
+        "id": "com.phonegap.plugins.GeLoCordovaPlugin.GeLoCordovaPlugin",
+        "clobbers": [
+            "GeLoCordovaPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.phonegap.plugins.GeLoCordovaPlugin": "2.1.1"
+}
+// BOTTOM OF METADATA
+});
