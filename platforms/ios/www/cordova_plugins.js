b0d95f4a566c3d078f88b25009e92ad85cc554ed
Javad Afshar <mjafshar@gmail.com>
2014-05-01 12:44:21 -0500

    Rebuilt Phonegap project without bluetooth.

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,17 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.phonegap.plugins.GeLoCordovaPlugin/www/GeLoCordovaPlugin.js",
-        "id": "com.phonegap.plugins.GeLoCordovaPlugin.GeLoCordovaPlugin",
-        "clobbers": [
-            "GeLoCordovaPlugin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "com.phonegap.plugins.GeLoCordovaPlugin": "2.1.1"
-}
+{}
 // BOTTOM OF METADATA
 });
