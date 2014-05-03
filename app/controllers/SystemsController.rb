d0a7480f297720a76a8b1565314824486f6a4a43
Javad Afshar <mjafshar@gmail.com>
2014-05-03 16:32:46 -0500

    Updated sysytem controller and model to update view with info from db.

--- app/controllers/SystemsController.rb
+++ app/controllers/SystemsController.rb
@@ -3,11 +3,19 @@ class SystemsController < UIViewController
     super
     self.view.backgroundColor = UIColor.whiteColor
     @label = UILabel.alloc.initWithFrame(CGRectZero)
-    system = System.pull_system_data(1)
-    puts "========================"
-    p system
-    puts "========================"
-    self.title = "Systems"
+    system = System.pull_system_data(1) do |system|
+      if system.nil?
+        self.title = "Error"
+        @label.text = "Error"
+      else
+        puts "=================="
+        p system
+        puts "=================="
+        self.title = system[:name]
+        @label.text = system[:description]
+      end
+    end
+    # self.title = "Systems"
     @label.text = "Awesome System"
     @label.sizeToFit
     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
--- app/models/system.rb
+++ app/models/system.rb
@@ -12,21 +12,19 @@ class System
     }
   end
 
-  def self.pull_system_data(system_id)
+  def self.pull_system_data(system_id, &block)
     BW::HTTP.get("http://192.168.0.9:3000/locations/#{system_id}") do |response|
       result_data = BW::JSON.parse(response.body.to_str)
-      puts "result_data"
-      @system_data = result_data[:system]
-      puts "Inside: #{@system_data}"
-      # if system.id.to_id == -1
+      system_data = result_data[:system]
+      # puts "=================="
+      # p system_data
+      # puts "=================="
+      block.call(system_data)
+      # if system.id.to_i == -1
       #   block.call(nil)
       # else
-      #   block.call(system)
+      #   block.call(system_data)
       # end
-
-      # p response.body.to_str
-      # block.call(nil)
     end
-    puts "Outside: #{@system_data}"
   end
 end
