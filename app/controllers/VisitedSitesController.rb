6d0915aef76fdfa00f778da4fe3fe27e872695d0
Javad Afshar <mjafshar@gmail.com>
2014-05-03 20:44:05 -0500

    Added user_id variable to pass into .visited_sites method. Hard coded until Twitter users are up.

--- app/controllers/VisitedSitesController.rb
+++ app/controllers/VisitedSitesController.rb
@@ -8,7 +8,8 @@ class VisitedSitesController < UIViewController
     @label.sizeToFit
     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
     @user = User.new
-    @user.visited_sites(1) do |visits|
+    user_id = 1
+    @user.visited_sites(user_id) do |visits|
       @location_names = visits.values
       @visits = visits
       @table = UITableView.alloc.initWithFrame(self.view.bounds)
