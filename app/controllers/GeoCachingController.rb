8159ced122f3f35c12bbd7e6fa4ecb463035b7b6
Javad Afshar <mjafshar@gmail.com>
2014-05-03 22:55:35 -0500

    Added camera functionality.

--- app/controllers/GeoCachingController.rb
+++ app/controllers/GeoCachingController.rb
@@ -8,6 +8,18 @@ class GeoCachingController < UIViewController
     @label.sizeToFit
     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
     self.view.addSubview(@label)
+
+    @picture_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @picture_button.setTitle("Take A Pic", forState:UIControlStateNormal)
+    @picture_button.sizeToFit
+    # @picture_button.center = CGPointMake(self.view.frame.size.width / 2, @text_field.center.y + 40)
+    self.view.addSubview @picture_button
+
+    @picture_button.when(UIControlEventTouchUpInside) do
+      # @picture_button.enabled = false
+
+      take_picture
+    end
   end
 
   def initWithNibName(name, bundle: bundle)
@@ -15,4 +27,16 @@ class GeoCachingController < UIViewController
     self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemDownloads, tag: 2)
     self
   end
+
+  def take_picture
+    BW::Device.camera.rear.picture(media_types: [:movie, :image]) do |result|
+      image_view = UIImageView.alloc.initWithImage(result[:original_image])
+    end
+  end
+
+  # def take_picture
+  #   BW::Device.camera.any.picture(media_types: [:movie, :image]) do |result|
+  #     image_view = UIImageView.alloc.initWithImage(result[:original_image])
+  #   end
+  # end
 end
