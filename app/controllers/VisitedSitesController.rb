87924ad518ebe1578a08e50d3e87c09ec5541a39
Javad Afshar <mjafshar@gmail.com>
2014-05-03 20:21:31 -0500

    Updated PROPERTIES with the correct DB attributes
    Created new user and called visited sites.
    
    Moved view code into callback block, and defined the proper table text.
    
    Initializing SystemsController with the correct location id.

--- app/controllers/VisitedSitesController.rb
+++ app/controllers/VisitedSitesController.rb
@@ -7,11 +7,15 @@ class VisitedSitesController < UIViewController
     @label.text = "Where you've visited"
     @label.sizeToFit
     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
-    @table = UITableView.alloc.initWithFrame(self.view.bounds)
-    self.view.addSubview(@table)
-    @table.dataSource = self
-    @table.delegate = self
-    @data = ("A".."Z").to_a
+    @visits = User.new
+    @visits.visited_sites(1) do |visits|
+      @location_names = visits.values
+      @visits = visits
+      @table = UITableView.alloc.initWithFrame(self.view.bounds)
+      self.view.addSubview(@table)
+      @table.dataSource = self
+      @table.delegate = self
+    end
   end
 
   def initWithNibName(name, bundle: bundle)
@@ -29,24 +33,22 @@ class VisitedSitesController < UIViewController
         reuseIdentifier:@reuseIdentifier)
     end
 
-    cell.textLabel.text = @data[indexPath.row]
+    cell.textLabel.text = @location_names[indexPath.row]
 
     cell
   end
 
   def tableView(tableView, numberOfRowsInSection: section)
-    @data.count
+    @location_names.count
   end
 
   def tableView(tableView, didSelectRowAtIndexPath:indexPath)
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
-    letter = sections[indexPath.section]
+    location_name = @location_names[indexPath.row]
+    location = @visits.select{ |key, value| value == location_name }
+    location_id = location.keys.first.to_i
 
-    systems_controller = SystemsController.alloc.initWithNibName(nil, bundle: nil)
+    systems_controller = SystemsController.alloc.initWithParams({system_id: location_id})
     self.navigationController.pushViewController(systems_controller, animated:true)
-  end
-
-  def sections
-    @data.sort
   end
 end
