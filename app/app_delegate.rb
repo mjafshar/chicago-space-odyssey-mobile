803efc72860b8072a5c29b7c93e31281152e4cd4
Jaime Schubert <jkschub@gmail.com>
2014-05-05 17:27:20 -0500

    fixed twitter check to see if allowed in settings

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,24 +4,35 @@ class AppDelegate
     @window.makeKeyAndVisible
 
     if Twitter::Composer.available?
-      puts "Twitter ID #{Twitter.accounts[0].user_id}"
-      @defaults = NSUserDefaults.standardUserDefaults
-      @defaults["twitter_id"] = Twitter.accounts[0].user_id
-      
-      exo_facts_controller = ExoFactsController.alloc.initWithNibName(nil, bundle: nil)
-      geo_cache_controller = GeoCachingController.alloc.initWithNibName(nil, bundle: nil)
-      @visited_sites_controller = VisitedSitesController.alloc.initWithNibName(nil, bundle: nil)
-      @visited_nav_controller = UINavigationController.alloc.initWithRootViewController(@visited_sites_controller)
-      adler_controller = AdlerInfoController.alloc.initWithNibName(nil, bundle: nil)
+      Twitter.sign_in do |granted, ns_error|
+        if granted
+          puts "Twitter ID #{Twitter.accounts[0].user_id}"
+          @defaults = NSUserDefaults.standardUserDefaults
+          @defaults["twitter_id"] = Twitter.accounts[0].user_id
+          
+          exo_facts_controller = ExoFactsController.alloc.initWithNibName(nil, bundle: nil)
+          geo_cache_controller = GeoCachingController.alloc.initWithNibName(nil, bundle: nil)
+          @visited_sites_controller = VisitedSitesController.alloc.initWithNibName(nil, bundle: nil)
+          @visited_nav_controller = UINavigationController.alloc.initWithRootViewController(@visited_sites_controller)
+          adler_controller = AdlerInfoController.alloc.initWithNibName(nil, bundle: nil)
 
-      tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-      tab_controller.viewControllers = [exo_facts_controller, geo_cache_controller, @visited_nav_controller, adler_controller]
+          tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+          tab_controller.viewControllers = [exo_facts_controller, geo_cache_controller, @visited_nav_controller, adler_controller]
 
-      @window.rootViewController = tab_controller
+          @window.rootViewController = tab_controller
+        else
+          label = UILabel.alloc.initWithFrame(CGRectZero)
+          @window.backgroundColor = UIColor.whiteColor
+          label.text = "Please allow access to Twitter in settings"
+          label.sizeToFit
+          label.center = CGPointMake(@window.frame.size.width / 2, @window.frame.size.height / 2)
+          @window.addSubview(label)
+        end
+      end
     else
       label = UILabel.alloc.initWithFrame(CGRectZero)
       @window.backgroundColor = UIColor.whiteColor
-      label.text = "Login to Twitter!"
+      label.text = "Login to Twitter in your settings!"
       label.sizeToFit
       label.center = CGPointMake(@window.frame.size.width / 2, @window.frame.size.height / 2)
       @window.addSubview(label)
