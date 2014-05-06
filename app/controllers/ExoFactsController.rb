87c3aab8215a9270a333de8b0a68c8a56960bad5
Amelia Padua <amelia.padua@gmail.com>
2014-05-05 19:03:13 -0500

    send user id and location id to server when user opens app

--- app/controllers/ExoFactsController.rb
+++ app/controllers/ExoFactsController.rb
@@ -10,7 +10,7 @@ class ExoFactsController < UIViewController
     @add_button.sizeToFit
     @add_button.frame = CGRect.new([10, 50], @add_button.frame.size)
     @add_button.addTarget(self, action:"display_lat_long", forControlEvents:UIControlEventTouchUpInside)
-    self.view.addSubview(@add_button)
+    # self.view.addSubview(@add_button)
 
     @adler_planetarium = CLLocationCoordinate2D.new(41.866333, -87.606783)
     @soldier_field = CLLocationCoordinate2D.new(41.862074, -87.616804)
@@ -22,15 +22,15 @@ class ExoFactsController < UIViewController
     @wrigley = CLLocationCoordinate2D.new(41.947854, -87.655642)
     @pile = CLLocationCoordinate2D.new(41.792015, -87.599959)
 
-    @adler_planetarium_region = CLCircularRegion.alloc.initWithCenter(@adler_planetarium, radius: 50, identifier:"Adler Planetarium")
-    @soldier_field_region = CLCircularRegion.alloc.initWithCenter(@soldier_field, radius: 50, identifier:"Soldier Field")
-    @ferris_wheel_region = CLCircularRegion.alloc.initWithCenter(@ferris_wheel, radius: 50, identifier:"Navy Pier Ferris Wheel")
-    @mill_park_region = CLCircularRegion.alloc.initWithCenter(@mill_park, radius: 50, identifier:"Millenium Park")
-    @merch_mart_region = CLCircularRegion.alloc.initWithCenter(@merch_mart, radius: 50, identifier:"Merchandise Mart")
-    @dbc_region = CLCircularRegion.alloc.initWithCenter(@dbc, radius: 50, identifier:"Dev Bootcamp")
-    @us_cell_region = CLCircularRegion.alloc.initWithCenter(@us_cell, radius: 50, identifier:"US Cellular Field")
-    @wrigley_region = CLCircularRegion.alloc.initWithCenter(@wrigley, radius: 50, identifier:"Wrigley Field")
-    @pile_region = CLCircularRegion.alloc.initWithCenter(@pile, radius: 50, identifier:"Chicago Pile-1")
+    @adler_planetarium_region = CLCircularRegion.alloc.initWithCenter(@adler_planetarium, radius: 100, identifier:"Adler Planetarium")
+    @soldier_field_region = CLCircularRegion.alloc.initWithCenter(@soldier_field, radius: 100, identifier:"Soldier Field")
+    @ferris_wheel_region = CLCircularRegion.alloc.initWithCenter(@ferris_wheel, radius: 100, identifier:"Navy Pier Ferris Wheel")
+    @mill_park_region = CLCircularRegion.alloc.initWithCenter(@mill_park, radius: 100, identifier:"Millenium Park")
+    @merch_mart_region = CLCircularRegion.alloc.initWithCenter(@merch_mart, radius: 100, identifier:"Merchandise Mart")
+    @dbc_region = CLCircularRegion.alloc.initWithCenter(@dbc, radius: 100, identifier:"Dev Bootcamp")
+    @us_cell_region = CLCircularRegion.alloc.initWithCenter(@us_cell, radius: 100, identifier:"US Cellular Field")
+    @wrigley_region = CLCircularRegion.alloc.initWithCenter(@wrigley, radius: 100, identifier:"Wrigley Field")
+    @pile_region = CLCircularRegion.alloc.initWithCenter(@pile, radius: 100, identifier:"Chicago Pile-1")
 
     @all_regions = [@adler_planetarium_region, @soldier_field_region, @ferris_wheel_region, @mill_park_region, @merch_mart_region, @dbc_region, @us_cell_region, @wrigley_region, @pile_region]
 
@@ -74,7 +74,7 @@ class ExoFactsController < UIViewController
           @location_manager.monitoredRegions.each {|region| @locations << region}
 
           @regionStateArray << @locations.find do |region|
-            calculateDistance(@user_coords, region.center) < 50
+            calculateDistance(@user_coords, region.center) < 100
           end
 
           if @regionStateArray.first != nil
@@ -90,23 +90,25 @@ class ExoFactsController < UIViewController
 
                 self.view.backgroundColor = UIColor.whiteColor
                 # systems_controller = SystemsController.alloc.initWithParams({location_id: location_id})
-                    System.pull_system_data(location_id) do |system|
-                      self.title = system[:name]
+                System.pull_system_data(location_id) do |system|
+                  self.title = system[:name]
 
-                      frame = UIScreen.mainScreen.applicationFrame
-                      origin = frame.origin
-                      size = frame.size
-                      body = UITextView.alloc.initWithFrame([[origin.x, origin.y + 20], [size.width, size.height]])
-                      body.text = system[:description]
-                      body.editable = false
-                      scroll_view = UIScrollView.alloc.initWithFrame(frame)
+                  frame = UIScreen.mainScreen.applicationFrame
+                  origin = frame.origin
+                  size = frame.size
+                  body = UITextView.alloc.initWithFrame([[origin.x, origin.y + 20], [size.width, size.height]])
+                  body.text = system[:description]
+                  body.editable = false
+                  scroll_view = UIScrollView.alloc.initWithFrame(frame)
 
-                      scroll_view.showsVerticalScrollIndicator = true
-                      scroll_view.scrollEnabled = true
-                      scroll_view.addSubview(body)
-                      scroll_view.contentSize = body.frame.size
-                      self.view.addSubview(scroll_view)
-                    end
+                  scroll_view.showsVerticalScrollIndicator = true
+                  scroll_view.scrollEnabled = true
+                  scroll_view.addSubview(body)
+                  scroll_view.contentSize = body.frame.size
+                  self.view.addSubview(scroll_view)
+                end
+                register_user_and_location
+                
               end
             end
             # self.view.backgroundColor = UIColor.whiteColor
@@ -211,11 +213,6 @@ class ExoFactsController < UIViewController
     alert.message = "You have entered the region! Hooray!"
     alert.show
 
-    # notification = UILocalNotification.alloc.init
-    # notification.fireDate = NSDate.date
-    # notification.timeZone = NSTimeZone.defaultTimeZone
-    # notification.alertBody = "Entered Region"
-
   end
 
   def locationManager(manager, didExitRegion:region)
@@ -223,11 +220,6 @@ class ExoFactsController < UIViewController
     alert.addButtonWithTitle("OK")
     alert.message = "You left! Come back!"
     alert.show
-
-    # notification = UILocalNotification.alloc.init
-    # notification.fireDate = NSDate.date
-    # notification.timeZone = NSTimeZone.defaultTimeZone
-    # notification.alertBody = "Exited Region"
   end
 
   def locationManager(manager, monitoringDidFailForRegion:region, withError:error)
@@ -235,55 +227,6 @@ class ExoFactsController < UIViewController
     alert.addButtonWithTitle("OK")
     alert.message = error
     alert.show
-  end
-
-  def locationManager(manager, didDetermineState: state, forRegion: region)
-    puts "just before iiffffsss"
-    @regionStateArray << state
-    puts "LOOK HERE!!!! #{@regionStateArray}"
-
-    if state == CLRegionStateInside
-      puts "inside state: #{state}"
-      puts "Region inside state: #{CLRegionStateInside}"
-      puts "Region: #{region.identifier}"
-      puts "we're inside YO!"
-      general_alert("inside the region! #{region.identifier}")
-
-      # @label = UILabel.alloc.initWithFrame(CGRectZero)
-      # self.title = "Exo System"
-      # @label.text = 'Exo-system Facts'
-      # @label.sizeToFit
-      # @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
-      # self.view.addSubview(@label)
-    # elsif state == CLRegionStateOutside
-    else
-      puts "outside state: #{state}"
-      puts "Region: #{region.identifier}"
-      general_alert("outside the region #{region.identifier} :(")
-      puts "We're outside YO!"
-
-        # if !@regionStateArray.include?(1)
-        #   map = MapView.new
-        #   map.frame = self.view.frame
-        #   map.delegate = self
-        #   map.region = CoordinateRegion.new([41.8337329, -87.7321555], [1, 1])
-        #   map.shows_user_location = true
-        #   map.zoom_enabled = true
-        #   map.scroll_enabled = true
-        #   view.addSubview(map)
-
-      # bull = MKPointAnnotation.new
-      # bull_coords = CLLocationCoordinate2D.new(41.8896956, -87.634291)
-      # bull.coordinate = bull_coords
-      # bull.title = "Bull and Bear"
-      # map.addAnnotation(bull)
-        # end
-    # else
-    #   general_alert("HEY HEYE HEHEYYEHHEYEYHEY HEY hi")
-    #   puts "another erroooorr?"
-    end
-
-    puts "wtf is happening at the END - THIS IS THE LAST LINE"
   end
 
   def show_error_message(message)
@@ -307,4 +250,15 @@ class ExoFactsController < UIViewController
     alert.show
   end
 
+  def register_user_and_location
+    data = {user_id: @defaults["twitter_id"], location_id: @defaults["user_location"]}
+    send_user_info_post_request(data)
+  end
+
+  def send_user_info_post_request(payload)
+    BW::HTTP.post("http://192.168.0.90:3000/users/mobile", {payload: payload}) do |response|
+    end
+  end
+
 end
+
